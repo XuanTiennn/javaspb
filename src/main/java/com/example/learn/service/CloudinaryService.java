35c00bbbccd0a931c8b5952f8f9dc09f2a28bb2e
xuantien <taxuantien312000@gmail.com>
2023-06-01 21:33:29 +0700

    commit

--- src/main/java/com/example/learn/service/CloudinaryService.java
+++ src/main/java/com/example/learn/service/CloudinaryService.java
@@ -7,6 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
+@Service
+
 public class CloudinaryService {
     private final Cloudinary cloudinary;
 
